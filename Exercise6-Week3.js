629303566dfcda9491c1e7b1b0af510a51a7fb3a
Handi Priyono <handimacbook@gmail.com>
2018-02-10 00:21:08 +0700

    Change method alternative

--- Exercise6-Week3.js
+++ Exercise6-Week3.js
@@ -1,24 +1,27 @@
 function angkaPalindrome(num) {
 
-  num++; // add num by 1
+  // add num by 1 for making next number
+  num = num + 1;
+
   for (var i = num; i < num * num; i++) {
+    var balik = String(i).split('');
+    var tmp = '';
 
-    var balik = String(i).split('').reverse().join(''); // reverse number
-
-    if (i === (Number(balik))) { // compare num with num-reverse
-
-      return i;
-      break;
+    // making num to be Reverse using:  FOR Loop
+    for (var y = balik.length - 1; y >= 0; y--) {
+      tmp += balik[y];
     }
+
+    if (i === (Number(tmp))) { // compare num with num-reverse
+      return i;
+    }
+
   }
 
 }
 
 // TEST CASES only:
 console.log(angkaPalindrome(8)); // 9
-console.log(angkaPalindrome(800)); // 808
-console.log(angkaPalindrome(10)); // 11
-console.log(angkaPalindrome(117)); // 121
-console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(802220)); // 803308
 console.log(angkaPalindrome(1000)); // 1001
-console.log(angkaPalindrome(2000)); // 1002
+console.log(angkaPalindrome(20111)); // 20202

aa1b2208ef9f7a0cbf623b6867c2bf3652eb8eb0
Handi Priyono <handimacbook@gmail.com>
2018-02-11 09:11:07 +0700

    update typo err

--- Exercise15-Week3.js
+++ Exercise15-Week3.js
@@ -6,32 +6,29 @@ function groupAnimals(animals) {
   for (var q = 0; q < animalUrut.length; q++) {
 
     var arrayTmp = [];
-    var isi = ","
     for (var r = 0; r < animalUrut.length; r++) {
 
-
-
       if (animalUrut[q].charAt(0) === animalUrut[r].charAt(0)) {
-        isi = animalUrut[r]
+        isi = animalUrut[r];
         arrayTmp.push(isi);
 
       }
 
     }
 
-    arrayHasil.push(arre);
-
+    arrayHasil.push(arrayTmp);
 
   }
 
-
+  // Make array unique and no duplicate
   var ArrayUnik = [];
   var itemsFound = {};
   for (var i = 0, l = arrayHasil.length; i < l; i++) {
-    var stringified = JSON.stringify(aee[i]);
+    var stringified = JSON.stringify(arrayHasil[i]);
     if (itemsFound[stringified]) {
       continue;
     }
+
     ArrayUnik.push(arrayHasil[i]);
     itemsFound[stringified] = true;
   }
@@ -42,6 +39,5 @@ function groupAnimals(animals) {
   return ArrayUnik;
 }
 
-
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'kodok', 'kurungan', 'kelabang', 'kardus', 'hda']));
-// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+// test case only
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'kodok', 'tikus', 'kelabang']));
